dd725ba470293fd3d06360e6bbee1ae64ea2044a
Sebastian M Zdroana <s.zdroana@gmail.com>
2018-02-17 11:01:05 +0200

    This is the first draft. k

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("What a program...");
+        System.out.print("Please enter a number: ");
+        int num = new Scanner(System.in).nextInt();
+        for (int i = 0; i < num; i++) {
+            System.out.println("What a program...");
+        }
     }
 }
